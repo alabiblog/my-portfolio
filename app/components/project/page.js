5c4cb3d6b4e5668e2fcd669150201dc020535675
alabiblog <alabiworldwideblog@gmail.com>
2025-09-07 23:39:50 +0100

    second update

--- app/components/project/page.js
+++ app/components/project/page.js
@@ -7,7 +7,7 @@ export default function Project(){
                 <h1 className="text-4xl md:text-5xl lg:text-5xl text-stone-700 "><span className="text-stone-400 md:text-5xl lg:text-5xl font-bold">/</span>My Work<span className="text-stone-400 md:text-5xl lg:text-5xl font-bold">.</span></h1>
             </div>
               <h1 className="text-1xl  md:text-2xl lg:text-2xl text-center mx-5">Few works i have done in the past.</h1>
-          <div className="grid py-10 md:grid-cols-3 lg:grid-cols-3 md:ml-20 md:py-20 lg:py-20 lg:ml-20">
+          <div className="grid ml-7 py-10 md:grid-cols-3 lg:grid-cols-3 md:ml-20 md:py-20 lg:py-20 lg:ml-20">
              <div className="mx-2 w-90 shadow-xl h-80 bg-white">
                 <div className="w-full h-50 shadow-xl bg-gray-200">
                    <h1 className="text-center py-17 text-3xl">ALABI</h1>
--- app/dashboard/profile/page.js
+++ app/dashboard/profile/page.js
@@ -1,6 +1,10 @@
+import { auth } from "firebase-admin";
 import Image from "next/image";
 
-export default function Profile(){
+
+export default async function Profile(){
+    const session = await auth();
+    console.log(session)
     return(
         <main className="min-h-screen shadow-xl mx-10 my-10 lg:mx-50 xl:mx-100">
             <div className="flex justify-center mt-3">
@@ -11,9 +15,9 @@ export default function Profile(){
            alt="profile"
            className="h-20 w-20 rounded-full"/>
            </div>
-           <p className="text-center border-b mt-5">Name:</p>
-           <p className="text-center border-b mt-5">Email:</p>
-           <p className="text-center border-b mt-5">User ID:</p>
+           <p className="text-center border-b mt-5">{session?.user?.name}</p>
+           <p className="text-center border-b mt-5">{session?.user?.name}</p>
+           <p className="text-center border-b mt-5">User ID: {session?.user?.name}</p>
         </main>
     )
 }
